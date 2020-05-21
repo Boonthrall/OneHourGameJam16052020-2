b1537f8835a755ac4494920f4e95a4a1636493e7
Shadowlight2017 <37117573+Shadowlight2017@users.noreply.github.com>
2020-05-21 16:24:06 +0200

    New Repository

---
+++ GameJam/Assets/Scripts/DimensionScripts/ChangeDimensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeDimensions : MonoBehaviour
+{
+    private GameObject[] red;
+    private GameObject[] blue;
+    private bool BlueIsActive = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       if(red == null)
+       {
+            getRedObjects();
+       }
+       if(blue == null)
+       {
+            getBlueObjects();
+       }
+    }
+
+    private void OnMouseDown()
+    {
+        BlueIsActive = !BlueIsActive;
+
+        if (BlueIsActive)
+        {
+            foreach(GameObject box in red)
+            {
+                box.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach(GameObject box in blue)
+            {
+                box.SetActive(false);
+            }
+        }
+    }
+
+    private void getRedObjects()
+    {
+        red = GameObject.FindGameObjectsWithTag("DimRed");
+    }
+    private void getBlueObjects()
+    {
+        blue = GameObject.FindGameObjectsWithTag("DimBlue");
+    }
+}
---
+++ GameJam/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    private Rigidbody2D rb;
+    private float move;
+    public float jumpHight = 5;
+    public int activeScene = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+        jump();
+    }
+
+    private void FixedUpdate()
+    {
+        walk();
+        jump();
+    }
+
+    private void walk()
+    {
+        Vector3 movement = new Vector3(move, 0f, 0f);
+        transform.position += movement * Time.deltaTime * movementSpeed;
+    }
+    private void jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHight), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Pit")
+        {
+            die();
+        }
+    }
+
+    private void die()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: activeScene);
+    }
+}
